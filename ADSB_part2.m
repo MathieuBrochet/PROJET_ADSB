81259d75bc12e1b19214338df1edcf42812ebe9c
Unknown <edronet@enseirb-matmeca.fr>
2018-11-09 14:29:58 +0100

    structure

--- ADSB_part2.m
+++ ADSB_part2.m
@@ -7,3 +7,14 @@ clc;
 
 %% 
 
+registre = struct ( 'adresse', [], ...           % de 9 à 32   
+                    'format', [], ...            % de 1 à 5
+                    'type', [], ...              % de 33 à 38
+                    'nom', [], ...               % 
+                    'altitude', [], ...          % de 41 à 52
+                    'timeFlag', [], ...          % 53
+                    'cprFlag', [], ...           % 54
+                    'latitude', [], ...          % de 55 à 71
+                    'longitude', [], ...         % de 72 à 88
+                    'trajectoire', [] );         % 
+               
