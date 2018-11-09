cf59eaa708a2a3071f9472de6d17f71af9232549
Unknown <math.brochet@hotmail.fr>
2018-11-09 22:44:38 +0100

    debug

--- decodage.m
+++ decodage.m
@@ -1,20 +1,24 @@
-function [message] = decodage(vecteur)
 
-    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    entiers = '0123456789';
-    size = 6;
-    len_vecteur = length(vecteur);
-    
-    for i=1:(len_vecteur/size)-1
-       valeur = bi2de(vecteur(i*size:i*(size+1)), 'left-msb')
-       
-       if valeur >0 && valeur <=26
-           message(i) = alphabet(valeur);
-       elseif valeur == 32
-           message(i) = ' ';
-       elseif valeur >=48 && valeur <=57
-           message(i) = entiers(valeur-47);
-       end
-    end   
-           
+function [message] = decodage(vecteur) 
+ 
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+    entiers = '0123456789'; 
+    size = 6; 
+    len_vecteur = length(vecteur); 
+     
+    for i=1:size:(len_vecteur) 
+         
+        
+       valeur = bi2de(vecteur(i:i+size-1), 'left-msb') 
+        
+       if valeur >0 && valeur <=26 
+           message(i) = alphabet(valeur); 
+       elseif valeur == 32 
+           message(i) = ' '; 
+       elseif valeur >=48 && valeur <=57 
+           message(i) = entiers(valeur-47); 
+       end 
+    end    
+     
+            
 end
